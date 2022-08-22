d0e309395e838ab37fc7d17d3f0a563ada29645f
Sanskar Agrawal <sanskar@ulesson.com>
2022-08-22 14:40:58 +0530

    Add androidx splash screen

--- app/src/main/java/dev/sanskar/pokedex/MainActivity.kt
+++ app/src/main/java/dev/sanskar/pokedex/MainActivity.kt
@@ -1,12 +1,19 @@
 package dev.sanskar.pokedex
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
