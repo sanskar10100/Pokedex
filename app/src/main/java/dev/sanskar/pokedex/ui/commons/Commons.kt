248bc1bd0716aaeae59caa9b6186f3389a27a6bf
Sanskar Agrawal <sanskar@ulesson.com>
2022-08-23 18:04:11 +0530

    Add swipe to remove item from list

--- app/src/main/java/dev/sanskar/pokedex/ui/commons/Commons.kt
+++ app/src/main/java/dev/sanskar/pokedex/ui/commons/Commons.kt
@@ -34,16 +34,20 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -51,6 +55,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -95,47 +100,71 @@ private fun ListHeader(header: String = "Tap on a pokemon to know more about it!
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
 @Composable
-private fun ListItem(pokemon: PokemonDetail?, loading: Boolean = false, onClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .placeholder(
-                visible = loading,
-                highlight = PlaceholderHighlight.shimmer()
-            ),
-        onClick = {
-            onClick()
+private fun ListItem(
+    pokemon: PokemonDetail?,
+    loading: Boolean = false,
+    onRemove: (Int) -> Unit,
+    onClick: () -> Unit,
+) {
+    SwipeToDismiss(
+        state = rememberDismissState {
+            if (it == DismissValue.DismissedToEnd) {
+                onRemove(pokemon?.id ?: -1)
+                true
+            } else false
         },
-        elevation = 3.dp,
-        shape = RoundedCornerShape(16.dp)
+        directions = setOf(DismissDirection.StartToEnd),
+        background = {
+            Box(
+                Modifier
+                    .padding(8.dp)
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color(0xFFFF6F00))
+            )
+        }
     ) {
-        AnimatedContent(targetState = loading, transitionSpec = {
-            fadeIn() + slideIntoContainer(towards = AnimatedContentScope.SlideDirection.Left,
-                animationSpec = tween(500)) with fadeOut()
-        }) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.background(Color(0xFF81D4FA))
-            ) {
-                Text(
-                    text = pokemon?.name?.replaceFirstChar { it.uppercase() } ?: "",
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp)
-                        .fillMaxWidth(),
-                )
-                Box(
-                    Modifier.background(Color(0xFFE6EE9C))
+        Card(
+            modifier = Modifier
+                .padding(8.dp)
+                .placeholder(
+                    visible = loading,
+                    highlight = PlaceholderHighlight.shimmer()
+                ),
+            onClick = {
+                onClick()
+            },
+            elevation = 3.dp,
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            AnimatedContent(targetState = loading, transitionSpec = {
+                fadeIn() + slideIntoContainer(towards = AnimatedContentScope.SlideDirection.Left,
+                    animationSpec = tween(500)) with fadeOut()
+            }) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.background(Color(0xFF81D4FA))
                 ) {
-                    SubcomposeAsyncImage(
-                        model = pokemon?.sprites?.front_default ?: "",
-                        contentDescription = "Image of ${pokemon?.name}",
-                        loading = {
-                            CircularProgressIndicator(modifier = Modifier.size(24.dp))
-                        },
-                        modifier = Modifier.size(72.dp),
-                        contentScale = ContentScale.FillBounds
+                    Text(
+                        text = pokemon?.name?.replaceFirstChar { it.uppercase() } ?: "",
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(8.dp)
+                            .fillMaxWidth(),
                     )
+                    Box(
+                        Modifier.background(Color(0xFFE6EE9C))
+                    ) {
+                        SubcomposeAsyncImage(
+                            model = pokemon?.sprites?.front_default ?: "",
+                            contentDescription = "Image of ${pokemon?.name}",
+                            loading = {
+                                CircularProgressIndicator(modifier = Modifier.size(24.dp))
+                            },
+                            modifier = Modifier.size(72.dp),
+                            contentScale = ContentScale.FillBounds
+                        )
+                    }
                 }
             }
         }
@@ -158,7 +187,7 @@ fun PokemonsList(
         mutableStateListOf(
             PokemonDetail(0,
                 0,
-                0,
+                1,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -168,7 +197,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                2,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -178,7 +207,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                3,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -188,7 +217,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                4,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -198,7 +227,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                5,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -208,7 +237,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                6,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -218,7 +247,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                7,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -228,7 +257,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                8,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -238,7 +267,7 @@ fun PokemonsList(
                 20),
             PokemonDetail(0,
                 0,
-                0,
+                9,
                 "",
                 listOf(Move(NameData("", ""))),
                 1,
@@ -255,8 +284,8 @@ fun PokemonsList(
         stickyHeader {
             if (headerText.isNotEmpty()) ListHeader(headerText) else ListHeader()
         }
-        items(pokemonsList) { item ->
-            ListItem(item, loading) {
+        items(pokemonsList, key = { it.id }) { item ->
+            ListItem(item, loading, { pokemonId -> pokemonsList.removeAll {it.id == pokemonId}}) {
                 if (!loading) onListItemClicked(item.id) else {
 
                 }
